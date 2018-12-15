54e481e82b0caa4ed657da0b4bd1e27b82e2fbfb
mandzeete <aivar.loopalu@gmail.com>
2018-12-15 17:38:55 +0200

    muudatused laud.sql sees

--- laud.sql
+++ laud.sql
@@ -108,7 +108,6 @@ CREATE TABLE Isik
   CONSTRAINT AK_Isik_e_meil UNIQUE (e_meil),
   CONSTRAINT AK_Isikukood_riik UNIQUE (isikukood,isikukoodi_riik),
   CONSTRAINT isik_e_meil_check_oige_vorm CHECK (e_meil~'^[a-z0-9.]+@[a-z0-9.]+[a-z]+$'),
-  CONSTRAINT isik_isikukood_check_lubatud_symbolid CHECK (isikukood~'[a-z0-9[:space:]\\-.]+'),
   CONSTRAINT isik_synni_kp_check_lubatud_vahemik CHECK (synni_kp >= '01.01.1900' AND synni_kp <= '12.31.2100'),
   CONSTRAINT isik_synni_kp_check_v2iksem_v6rdne_isiku_registreerimise_ajast CHECK (synni_kp <= reg_kp),
   CONSTRAINT isik_reg_kp_check_lubatud_vahemik CHECK (reg_kp >= '01.01.2010' AND reg_kp < '01.01.2101'),
@@ -169,19 +168,17 @@ ALTER TABLE Laua_seisundi_liik OWNER TO t164416;
 
 CREATE TABLE Laud
 (
-  laua_id integer NOT NULL,
+  laua_kood integer NOT NULL,
   tootaja_id smallint NOT NULL,
   laua_seisundi_liik_kood integer NOT NULL DEFAULT 1,
   laua_materjal_kood integer,
-  laua_kood integer NOT NULL,
   reg_kp date NOT NULL DEFAULT CURRENT_DATE,
   kohtade_arv Integer NOT NULL,
   kommentaar varchar(255)	,
-  CONSTRAINT PK_Laud PRIMARY KEY (laua_id),
+  CONSTRAINT PK_laua_kood PRIMARY KEY (laua_kood),
   CONSTRAINT laud_kohtade_arv_check_suurem_yhest CHECK (kohtade_arv > 1),
   CONSTRAINT laud_reg_kp_check_lubatud_vahemik CHECK (reg_kp >= '01.01.2010' AND reg_kp < '01.01.2101'),
   CONSTRAINT laud_kommentaar_check_ei_ole_tyhi_string CHECK (kommentaar!~'^[[:space:]]*$'),
-  CONSTRAINT AK_laua_kood UNIQUE (laua_kood),
   CONSTRAINT laud_reg_kp_check_v2iksem_v6rdne_kui_hetke_kuupaev CHECK (reg_kp <= CURRENT_DATE),
   CONSTRAINT FK_Laud_Laua_materjal FOREIGN KEY (laua_materjal_kood) REFERENCES Laua_materjal (laua_materjal_kood) ON DELETE No Action ON UPDATE Cascade,
   CONSTRAINT FK_Laud_Laua_seisundi_liik FOREIGN KEY (laua_seisundi_liik_kood) REFERENCES Laua_seisundi_liik (laua_seisundi_liik_kood) ON DELETE No Action ON UPDATE Cascade,
@@ -220,11 +217,11 @@ ALTER TABLE Laua_kategooria OWNER TO t164416;
 CREATE TABLE Laua_kategooria_omamine
 (
   laua_kategooria_omamine_id integer NOT NULL,
-  laua_id integer NOT NULL,
+  laua_kood integer NOT NULL,
   laua_kategooria_kood integer NOT NULL,
   CONSTRAINT PK_laua_kategooria_omamine PRIMARY KEY (laua_kategooria_omamine_id),
-  CONSTRAINT AK_laua_kategooria_omamine_Laua_katogooria_Laud UNIQUE (laua_id,laua_kategooria_kood),
-  CONSTRAINT FK_laua_kategooria_omamine_Laud FOREIGN KEY (laua_id) REFERENCES Laud (laua_id) ON DELETE Cascade ON UPDATE Cascade,
+  CONSTRAINT AK_laua_kategooria_omamine_Laua_katogooria_Laud UNIQUE (laua_kood,laua_kategooria_kood),
+  CONSTRAINT FK_laua_kategooria_omamine_Laud FOREIGN KEY (laua_kood) REFERENCES Laud (laua_kood) ON DELETE Cascade ON UPDATE Cascade,
   CONSTRAINT FK_laua_kategooria_omamine_Laua_kategooria FOREIGN KEY (laua_kategooria_kood) REFERENCES Laua_kategooria (laua_kategooria_kood) ON DELETE No Action ON UPDATE No Action
 )
 ;
@@ -280,10 +277,9 @@ CREATE INDEX IXFK_Laud_Laua_materjal ON Laud (laua_materjal_kood ASC);
 CREATE INDEX IXFK_Laud_Laua_seisundi_liik ON Laud (laua_seisundi_liik_kood ASC);
 CREATE INDEX IXFK_Laua_kategooria_Laua_kategooria_tyyp ON Laua_kategooria (laua_kategooria_tyyp_kood ASC);
 CREATE INDEX IXFK_laua_kategooria_omamine_Laua_kategooria ON Laua_kategooria_omamine (laua_kategooria_kood ASC);
-CREATE INDEX IXFK_laua_kategooria_omamine_Laud ON Laua_kategooria_omamine (laua_id ASC);
+CREATE INDEX IXFK_laua_kategooria_omamine_Laud ON Laua_kategooria_omamine (laua_kood ASC);
 CREATE INDEX IXFK_Klient_Kliendi_seisundi_liik ON Klient (kliendi_seisundi_liik_kood ASC);
 CREATE INDEX IXFK_Klient_Isik ON Klient (isiku_id ASC);
-
 
 DROP VIEW IF EXISTS aktiivsed_ja_mitteaktiivsed_lauad;
 
@@ -302,8 +298,8 @@ WHERE Laud.laua_seisundi_liik_kood = Laua_seisundi_liik.laua_seisundi_liik_kood
 
 ALTER VIEW aktiivsed_ja_mitteaktiivsed_lauad OWNER TO t164416;
 
-
 DROP VIEW IF EXISTS koik_lauad;
+
 CREATE VIEW koik_lauad AS
 SELECT Laud.laua_kood,
        Laua_seisundi_liik.nimetus AS hetkeseisund,
@@ -321,7 +317,6 @@ FROM Laua_materjal,
 WHERE (((Laud.laua_materjal_kood) = Laua_materjal.laua_materjal_kood) And ((Laud.tootaja_id) = Isik.isiku_id));
 
 ALTER VIEW koik_lauad OWNER TO t164416;
-
 
 DROP VIEW IF EXISTS laudade_detailandmed;
 
@@ -342,11 +337,12 @@ FROM (Isik INNER JOIN (Tootaja INNER JOIN (Laud INNER JOIN Laua_materjal ON Laud
 
 ALTER VIEW laudade_detailandmed OWNER TO t164416;
 
-
+select *
+from laudade_detailandmed;
 
 DROP VIEW IF EXISTS laudade_kategooria_omamine;
 CREATE VIEW laudade_kategooria_omamine AS
-SELECT Laua_kategooria_omamine.laua_id,
+SELECT Laua_kategooria_omamine.laua_kood,
        Laua_kategooria.nimetus || ' (' || Laua_kategooria_tyyp.nimetus || ')' AS kategooria
 FROM Laua_kategooria_tyyp
        INNER JOIN (Laua_kategooria INNER JOIN Laua_kategooria_omamine ON Laua_kategooria.laua_kategooria_kood =
@@ -354,7 +350,6 @@ FROM Laua_kategooria_tyyp
                   ON Laua_kategooria_tyyp.laua_kategooria_tyyp_kood = Laua_kategooria.laua_kategooria_tyyp_kood;
 
 ALTER VIEW laudade_kategooria_omamine OWNER TO t164416;
-
 
 DROP VIEW IF EXISTS laudade_koondaruanne;
 CREATE VIEW laudade_koondaruanne AS
@@ -367,4 +362,3 @@ GROUP BY Laua_seisundi_liik.laua_seisundi_liik_kood, UPPER(Laua_seisundi_liik.ni
 ORDER BY Count(Laud.laua_kood) DESC, UPPER(Laua_seisundi_liik.nimetus);
 
 ALTER VIEW laudade_koondaruanne OWNER TO t164416;
-
