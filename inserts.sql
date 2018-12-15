4fe720bb5fe6413e5937a0f89e4e45c8c09ac9fd
Nils-Emil Lille <nils-emil.lille@nortal.com>
2018-12-15 17:23:40 +0200

    removed inserts from laud.sql and moved all selets to select.sql

--- inserts.sql
+++ inserts.sql
@@ -99,19 +99,3 @@ insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_k
 insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (11,6,2);
 insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (12,5,2);
 insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (13,3,1);
-
-
-select * from laua_kategooria_omamine;
-select * from tootaja;
-select * from klient;
-select * from isik;
-select * from laud;
-select * from laua_materjal;
-select * from laua_kategooria;
-select * from laua_kategooria_tyyp;
-select * from kliendi_seisundi_liik;
-select * from laua_seisundi_liik;
-select * from tootaja_seisundi_liik;
-select * from isiku_seisundi_liik;
-select * from amet;
-select * from riik;
--- laud.sql
+++ laud.sql
@@ -284,137 +284,6 @@ CREATE INDEX IXFK_laua_kategooria_omamine_Laud ON Laua_kategooria_omamine (laua_
 CREATE INDEX IXFK_Klient_Kliendi_seisundi_liik ON Klient (kliendi_seisundi_liik_kood ASC);
 CREATE INDEX IXFK_Klient_Isik ON Klient (isiku_id ASC);
 
-delete from riik;
-insert into riik (riik_kood, nimetus)values ('EST', 'Eesti');
-insert into riik (riik_kood, nimetus)values ('AFG', 'Afganistaan');
-insert into riik (riik_kood, nimetus)values ('RUS', 'Venemaa');
-insert into riik (riik_kood, nimetus)values ('GBR', 'Suurbritannia');
-insert into riik (riik_kood, nimetus)values ('ALB', 'Ahvenamaa');
-insert into riik (riik_kood, nimetus)values ('LVA', 'Läti');
-insert into riik (riik_kood, nimetus)values ('LTU', 'Leedu');
-insert into riik (riik_kood, nimetus)values ('DEU', 'Saksamaa');
-insert into riik (riik_kood, nimetus)values ('USA', 'Ameerika Ühendriigid');
-select * from riik;
-
-delete from amet;
-insert into amet (amet_kood, nimetus, kirjeldus)values (1, 'Kokk', 'Söögi tegemine');
-insert into amet (amet_kood, nimetus, kirjeldus)values (2, 'Kelner', 'Klientide teenindamine');
-insert into amet (amet_kood, nimetus, kirjeldus)values (3, 'Müüja', 'Kassa teenindamine');
-insert into amet (amet_kood, nimetus, kirjeldus)values (4, 'Koristaja', 'Tööpindade puhastuse operaator');
-insert into amet (amet_kood, nimetus, kirjeldus)values (5, 'Juhataja', 'Vastutav isik töökoha eest');
-select * from amet;
-
-delete from isiku_seisundi_liik;
-insert into isiku_seisundi_liik (isiku_seisundi_liik_kood, nimetus)values (1, 'Elus');
-insert into isiku_seisundi_liik (isiku_seisundi_liik_kood, nimetus)values (2, 'Surnud');
-select * from isiku_seisundi_liik;
-
-delete from tootaja_seisundi_liik;
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (1, 'Katseajal');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (2, 'Tööl');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (3, 'Puhkusel');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (4, 'Haiguslehel');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (5, 'Töösuhe peatatud');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (6, 'Töösuhe lõpetatud oma soovil');
-insert into tootaja_seisundi_liik (tootaja_seisundi_liik_kood, nimetus)values (7, 'Vallandatud');
-select * from tootaja_seisundi_liik;
-
-delete from laua_seisundi_liik;
-insert into laua_seisundi_liik (laua_seisundi_liik_kood, nimetus)values (1, 'Ootel');
-insert into laua_seisundi_liik (laua_seisundi_liik_kood, nimetus)values (2, 'Mitteaktiivne');
-insert into laua_seisundi_liik (laua_seisundi_liik_kood, nimetus)values (3, 'Aktiivne');
-insert into laua_seisundi_liik (laua_seisundi_liik_kood, nimetus)values (4, 'Lõpetatud');
-select * from laua_seisundi_liik;
-
-
-delete from kliendi_seisundi_liik;
-insert into kliendi_seisundi_liik (kliendi_seisundi_liik_kood, nimetus)values (1, 'Aktiivne');
-insert into kliendi_seisundi_liik (kliendi_seisundi_liik_kood, nimetus)values (2, 'Mustas nimekirjas');
-select * from kliendi_seisundi_liik;
-
-delete from laua_kategooria_tyyp;
-insert into laua_kategooria_tyyp (laua_kategooria_tyyp_kood, nimetus)values (1, 'Omadussõnad');
-insert into laua_kategooria_tyyp (laua_kategooria_tyyp_kood, nimetus)values (2, 'Liiklus laua ümber');
-insert into laua_kategooria_tyyp (laua_kategooria_tyyp_kood, nimetus)values (3, 'Vaade');
-select * from laua_kategooria_tyyp;
-
-
-delete from laua_kategooria;
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (1, 'Ilus', 1);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (2, 'Külmavõitu', 1);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (3, 'Hubane', 1);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (4, 'Palju', 2);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (5, 'Keskmiselt', 2);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (6, 'Vähe', 2);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (7, 'Ilus', 3);
-insert into laua_kategooria (laua_kategooria_kood, nimetus, laua_kategooria_tyyp_kood)values (8, 'kole', 3);
-select * from laua_kategooria;
-
-
-delete from laua_materjal;
-insert into laua_materjal (laua_materjal_kood, nimetus)values (1, 'Puit');
-insert into laua_materjal (laua_materjal_kood, nimetus)values (2, 'Klaas');
-insert into laua_materjal (laua_materjal_kood, nimetus)values (3, 'Metall');
-insert into laua_materjal (laua_materjal_kood, nimetus)values (4, 'Plastik');
-select * from laua_materjal;
-
-delete from isik;
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (1,'EST','nilsemil.lille@gmail.com',3554151,'03.20.2018','parool','Nils','Bob','Rakvere');
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (2,'GER','kaspar@gmail.com',5152151,'05.01.2018','parool','Kaspar','Bob','Tallinn');
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (3,'RUS','kerje@mail.ee',5125125,'05.01.2018','parool','Bob','Pajula','Keila');
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (4,'RUS','kaxim@max.ee',5512422,'04.01.2018','paroo','Maxim','Bob','Tallinn');
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (5,'RUS','karkus@ttu.ee',565122,'05.01.2018','password','Mihkel','Muhkel','Mägilinna');
-insert into isik (isiku_id, isikukoodi_riik, e_meil, isikukood, synni_kp, parool, eesnimi, perenimi, elukoht)values (6,'USA','hans@restoran.com',55555555,'11.28.2017','3213','Hans','Bob','Ei tea');
-select * from isik;
-
-
-delete from tootaja;
-insert into tootaja (isiku_id, amet_kood, mentor) values (1,1,2);
-insert into tootaja (isiku_id, amet_kood, mentor) values (3,3,3);
-insert into tootaja (isiku_id, amet_kood, mentor) values (4,4,4);
-insert into tootaja (isiku_id, amet_kood, mentor) values (5,2,2);
-insert into tootaja (isiku_id, amet_kood, mentor) values (6,2,3);
-select * from tootaja;
-
-
-delete from laud;
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (2,1,1,2,2,'Laud nagu laud ikka');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (3,2,2,3,2,'Laud nagu laud ikka');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (4,3,3,4,2,'Laud nagu laud ikka');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (5,4,3,5,2,'Laud nagu laud ikka');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (6,5,3,5,2,'Laud nagu laud ikka');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (7,6,2,5,2,'Eriline,habras.');
-insert into laud (laua_id, tootaja_id, laua_materjal_kood, laua_kood, kohtade_arv, kommentaar)values (8,6,1,3,2,'Ruumi on ka rohkemate jaoks');
-select * from laud;
-
-
-
-
-delete from klient;
-insert into klient (isiku_id, on_nous_tylitamisega, kliendi_seisundi_liik_kood) values (3,-1,1);
-insert into klient (isiku_id, on_nous_tylitamisega, kliendi_seisundi_liik_kood) values (4,0,1);
-insert into klient (isiku_id, on_nous_tylitamisega, kliendi_seisundi_liik_kood) values (5,-1,1);
-select * from klient;
-
-
-delete from tootaja;
-insert into tootaja (isiku_id, amet_kood, mentor) values (1,1,2);
-insert into tootaja (isiku_id, amet_kood, mentor) values (3,3,3);
-insert into tootaja (isiku_id, amet_kood, mentor) values (4,4,4);
-insert into tootaja (isiku_id, amet_kood, mentor) values (5,2,2);
-insert into tootaja (isiku_id, amet_kood, mentor) values (6,2,3);
-select * from tootaja;
-
-delete from laua_kategooria_omamine;
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (6,2,1);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (7,3,3);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (8,4,5);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (9,5,6);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (10,2,4);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (11,6,2);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (12,5,2);
-insert into laua_kategooria_omamine (laua_kategooria_omamine_id, laua_id, laua_kategooria_kood) values (13,3,1);
-select * from laua_kategooria_omamine;
 
 DROP VIEW IF EXISTS aktiivsed_ja_mitteaktiivsed_lauad;
 
@@ -433,12 +302,8 @@ WHERE Laud.laua_seisundi_liik_kood = Laua_seisundi_liik.laua_seisundi_liik_kood
 
 ALTER VIEW aktiivsed_ja_mitteaktiivsed_lauad OWNER TO t164416;
 
-select *
-from aktiivsed_ja_mitteaktiivsed_lauad;
-
 
 DROP VIEW IF EXISTS koik_lauad;
-
 CREATE VIEW koik_lauad AS
 SELECT Laud.laua_kood,
        Laua_seisundi_liik.nimetus AS hetkeseisund,
@@ -456,9 +321,6 @@ FROM Laua_materjal,
 WHERE (((Laud.laua_materjal_kood) = Laua_materjal.laua_materjal_kood) And ((Laud.tootaja_id) = Isik.isiku_id));
 
 ALTER VIEW koik_lauad OWNER TO t164416;
-
-select *
-from koik_lauad;
 
 
 DROP VIEW IF EXISTS laudade_detailandmed;
@@ -480,8 +342,7 @@ FROM (Isik INNER JOIN (Tootaja INNER JOIN (Laud INNER JOIN Laua_materjal ON Laud
 
 ALTER VIEW laudade_detailandmed OWNER TO t164416;
 
-select *
-from laudade_detailandmed;
+
 
 DROP VIEW IF EXISTS laudade_kategooria_omamine;
 CREATE VIEW laudade_kategooria_omamine AS
@@ -493,9 +354,6 @@ FROM Laua_kategooria_tyyp
                   ON Laua_kategooria_tyyp.laua_kategooria_tyyp_kood = Laua_kategooria.laua_kategooria_tyyp_kood;
 
 ALTER VIEW laudade_kategooria_omamine OWNER TO t164416;
-
-select *
-from laudade_kategooria_omamine;
 
 
 DROP VIEW IF EXISTS laudade_koondaruanne;
@@ -510,5 +368,3 @@ ORDER BY Count(Laud.laua_kood) DESC, UPPER(Laua_seisundi_liik.nimetus);
 
 ALTER VIEW laudade_koondaruanne OWNER TO t164416;
 
-select *
-from laudade_koondaruanne;
