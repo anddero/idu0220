96a50e6c474d5b5b01613af4b66e449464a95850
Nils-Emil Lille <nils-emil.lille@nortal.com>
2018-12-16 12:24:46 +0200

    add constraints to domain

--- domains.sql
+++ domains.sql
@@ -1,3 +1,5 @@
 DROP DOMAIN public.d_reg_kp;
 CREATE DOMAIN d_reg_kp date NOT NULL DEFAULT CURRENT_DATE;
+ALTER DOMAIN d_reg_kp ADD CONSTRAINT reg_kp_check_lubatud_vahemik CHECK (VALUE >= '01.01.2010' AND VALUE < '01.01.2101');
+ALTER DOMAIN d_reg_kp ADD CONSTRAINT reg_kp_check_v2iksem_v6rdne_kui_hetke_kuupaev CHECK (VALUE <= CURRENT_DATE);
 ALTER DOMAIN public.d_reg_kp OWNER to t164416;
--- laud.sql
+++ laud.sql
@@ -45,6 +45,8 @@ DROP SEQUENCE IF EXISTS seq_isik_isiku_id;
 
 DROP DOMAIN public.d_reg_kp;
 CREATE DOMAIN d_reg_kp date NOT NULL DEFAULT CURRENT_DATE;
+ALTER DOMAIN d_reg_kp ADD CONSTRAINT reg_kp_check_lubatud_vahemik CHECK (VALUE >= '01.01.2010' AND VALUE < '01.01.2101');
+ALTER DOMAIN d_reg_kp ADD CONSTRAINT reg_kp_check_v2iksem_v6rdne_kui_hetke_kuupaev CHECK (reg_kp <= CURRENT_DATE);
 ALTER DOMAIN public.d_reg_kp OWNER to t164416;
 
 
