36c7610000a2417b1351099eb6dc142084a362e5
Robin <rokopp@ttu.ee>
2018-12-16 14:20:43 +0200

    Rutiinile lisatud COMMENT

--- rutiin.sql
+++ rutiin.sql
@@ -8,25 +8,22 @@ RETURNING laua_kood;
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path=public, pg_temp;
 
-COMMENT ON FUNCTION f_lisa_laud(p_laua_kood integer, p_isiku_id integer, p_laua_materjal_kood integer, p_kohtade_arv integer, p_kommentaar varchar) IS 'Laua lisamine parameetrid on laua_kood, isiku_id, laua_materjal_kood, kohtade_arv ja kommmentaar';
 COMMENT ON FUNCTION f_lisa_laud IS 'Laua lisamine parameetrid on laua_kood, isiku_id, laua_materjal_kood, kohtade_arv ja kommmentaar';
 
-SELECT f_lisa_laud(p_laua_kood:=13, p_isiku_id:=3, p_laua_materjal_kood:=4, p_kohtade_arv:=3, p_kommentaar:='Kena laud!');
+--SELECT f_lisa_laud(p_laua_kood:=13, p_isiku_id:=3, p_laua_materjal_kood:=4, p_kohtade_arv:=3, p_kommentaar:='Kena laud!');
 
 CREATE OR REPLACE FUNCTION f_laua_kustutamine(p_laua_kood
-Laud.laua_kood%TYPE) RETURNS BOOLEAN
+Laud.laua_kood%TYPE) RETURNS
+Laud.laua_kood%TYPE
 AS $$
-BEGIN
-DELETE FROM Laud WHERE laua_kood=p_laua_kood;
---Kui kustutati vahemalt 1 rida, siis FOUND=TRUE
-RETURN FOUND;
-END; $$
-LANGUAGE plpgsql SECURITY DEFINER STRICT
+DELETE FROM Laud WHERE laua_kood=p_laua_kood
+RETURNING laua_kood;
+$$ LANGUAGE sql SECURITY DEFINER STRICT
 SET search_path = public, pg_temp;
 
 COMMENT ON FUNCTION f_laua_kustutamine IS 'Laua kood mida soovitakse kustutada parameetriks on laua_kood integer';
 
-SELECT f_lisa_laud(p_laua_kood:=13);
+--SELECT f_lisa_laud(p_laua_kood:=13);
 
 CREATE OR REPLACE FUNCTION f_muuda_laud (p_laua_kood_vana
 Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE,
@@ -41,7 +38,7 @@ SET search_path=public, pg_temp;
 
 COMMENT ON FUNCTION f_muuda_laud IS 'Laua muutmine parameetrid on laua_kood_vana, laua_kood_uus, isiku_id, kohtade_arv ja kommmentaar';
 
-SELECT f_muuda_laud(p_laua_kood_vana:=13,p_laua_kood_uus:=12,p_kohtade_arv:=5,p_kommentaar:='Uuem ja parem laud');
+--SELECT f_muuda_laud(p_laua_kood_vana:=13,p_laua_kood_uus:=12,p_kohtade_arv:=5,p_kommentaar:='Uuem ja parem laud');
 
 --Välja kutsumiseks SELECT f_laua_kustutamine(p_laua_id:=X); X=laua_id mida tahetakse kustutada
 
