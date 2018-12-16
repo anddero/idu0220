57a6536bebfb1f3a5ff0dcafc38bc73d46d5c0f1
Robin <rokopp@ttu.ee>
2018-12-16 13:42:27 +0200

    Rutiinile lisatud COMMENT

--- rutiin.sql
+++ rutiin.sql
@@ -8,6 +8,9 @@ RETURNING laua_kood;
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path=public, pg_temp;
 
+COMMENT ON FUNCTION f_lisa_laud(p_laua_kood integer, p_isiku_id integer, p_laua_materjal_kood integer, p_kohtade_arv integer, p_kommentaar varchar) IS 'Laua lisamine parameetrid on laua_kood, isiku_id, laua_materjal_kood, kohtade_arv ja kommmentaar';
+COMMENT ON FUNCTION f_lisa_laud IS 'Laua lisamine parameetrid on laua_kood, isiku_id, laua_materjal_kood, kohtade_arv ja kommmentaar';
+
 SELECT f_lisa_laud(p_laua_kood:=13, p_isiku_id:=3, p_laua_materjal_kood:=4, p_kohtade_arv:=3, p_kommentaar:='Kena laud!');
 
 CREATE OR REPLACE FUNCTION f_laua_kustutamine(p_laua_kood
@@ -16,10 +19,13 @@ AS $$
 BEGIN
 DELETE FROM Laud WHERE laua_kood=p_laua_kood;
 --Kui kustutati vahemalt 1 rida, siis FOUND=TRUE
+COMMENT ON TABLE Laud.laua_kood IS 'Kustutatav laud';
 RETURN FOUND;
 END; $$
 LANGUAGE plpgsql SECURITY DEFINER STRICT
 SET search_path = public, pg_temp;
+
+COMMENT ON FUNCTION f_laua_kustutamine IS 'Laua kood mida soovitakse kustutada parameetriks on laua_kood integer';
 
 SELECT f_lisa_laud(p_laua_kood:=13);
 
@@ -32,7 +38,9 @@ UPDATE Laud SET laua_kood=p_laua_kood_uus,
 isiku_id=p_isiku_id, kohtade_arv=p_kohtade_arv, kommentaar=p_kommentaar
 WHERE laua_kood=p_laua_kood_vana;
 $$ LANGUAGE SQL SECURITY DEFINER
-SET search_path=public, pg_temp;
+SET search_path=private, pg_temp;
+
+COMMENT ON FUNCTION f_muuda_laud IS 'Laua muutmine parameetrid on laua_kood_vana, laua_kood_uus, isiku_id, kohtade_arv ja kommmentaar';
 
 SELECT f_muuda_laud(p_laua_kood_vana:=13,p_laua_kood_uus:=12,p_kohtade_arv:=5,p_kommentaar:='Uuem ja parem laud');
 
