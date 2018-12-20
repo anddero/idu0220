3f9e1dfd59b0713963f2e253a346cf82562134c6
Nils-Emil Lille <nils-emil.lille@nortal.com>
2018-12-21 00:08:38 +0200

    fix

--- laud.sql
+++ laud.sql
@@ -82,7 +82,7 @@ DROP SEQUENCE IF EXISTS seq_laua_kategooria_omamine_id;
 DROP SEQUENCE IF EXISTS seq_isik_isiku_id;
 
 DROP DOMAIN IF EXISTS public.d_reg_aeg;
-CREATE DOMAIN d_reg_aeg date NOT NULL DEFAULT CURRENT_DATE;
+CREATE DOMAIN d_reg_aeg current_timestamp NOT NULL DEFAULT LOCALTIMESTAMP(0);
 ALTER DOMAIN d_reg_aeg ADD CONSTRAINT reg_aeg_check_lubatud_vahemik CHECK (VALUE >= '01.01.2010  00:00:00''' AND VALUE < '01.01.2101  00:00:00''');
 ALTER DOMAIN d_reg_aeg ADD CONSTRAINT reg_aeg_check_v2iksem_v6rdne_kui_hetke_aeg CHECK (VALUE <= LOCALTIMESTAMP(0));
 ALTER DOMAIN public.d_reg_aeg OWNER to t164416;
