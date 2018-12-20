1f3fa54b288a45a5486a2def0038ca73749b333c
Robin <rokopp@ttu.ee>
2018-12-20 13:11:41 +0200

    Finally comments work

--- rutiin.sql
+++ rutiin.sql
@@ -11,7 +11,7 @@ SET search_path=public, pg_temp;
 COMMENT ON FUNCTION f_lisa_laud(p_laua_kood Laud.laua_kood%TYPE, p_isiku_id Laud.isiku_id%TYPE,
 p_laua_materjal_kood Laud.laua_materjal_kood%TYPE,p_kohtade_arv Laud.kohtade_arv%type,
 p_kommentaar Laud.kommentaar%TYPE) IS
-'Protseduuri oodatavad
+'OP1 Protseduuri oodatavad
 sisendid on laua identifikaator (parameeter p_laua_kood),
 isiku identifikaator (parameeter p_isiku_id),
 laua materjali identifikaator (parameeter p_laua_materjal_kood),
@@ -20,7 +20,7 @@ kommentaar (parameeter p_kommentaar).
 Protseduur lisab laua.';
 --SELECT f_lisa_laud(p_laua_kood:=13, p_isiku_id:=3, p_laua_materjal_kood:=4, p_kohtade_arv:=3, p_kommentaar:='Kena laud!');
 
-CREATE OR REPLACE FUNCTION f_laua_kustutamine(p_laua_kood
+CREATE OR REPLACE FUNCTION f_laua_unustamine(p_laua_kood
 Laud.laua_kood%TYPE) RETURNS
 Laud.laua_kood%TYPE
 AS $$
@@ -29,8 +29,8 @@ RETURNING laua_kood;
 $$ LANGUAGE sql SECURITY DEFINER STRICT
 SET search_path = public, pg_temp;
 
-COMMENT ON FUNCTION f_laua_kustutamine(p_laua_kood
-Laud.laua_kood%TYPE) IS 'Protseduuri oodatavad
+COMMENT ON FUNCTION f_laua_unustamine(p_laua_kood
+Laud.laua_kood%TYPE) IS ' OP2 Protseduuri oodatavad
 sisendid on laua identifikaator (parameeter p_laua_kood).
 Protseduur kustutab etteantud laua.';
 
@@ -38,20 +38,18 @@ Protseduur kustutab etteantud laua.';
 
 CREATE OR REPLACE FUNCTION f_muuda_laud (p_laua_kood_vana
 Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE,
-p_isiku_id Laud.isiku_id%TYPE, p_kohtade_arv
-Laud.kohtade_arv%type, p_kommentaar Laud.kommentaar%TYPE)
+p_kohtade_arv Laud.kohtade_arv%type, p_kommentaar Laud.kommentaar%TYPE)
 RETURNS VOID AS $$
 UPDATE Laud SET laua_kood=p_laua_kood_uus,
-isiku_id=p_isiku_id, kohtade_arv=p_kohtade_arv, kommentaar=p_kommentaar
+kohtade_arv=p_kohtade_arv, kommentaar=p_kommentaar
 WHERE laua_kood=p_laua_kood_vana;
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path=public, pg_temp;
 
 COMMENT ON FUNCTION f_muuda_laud (p_laua_kood_vana
-Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE,
-p_isiku_id Laud.isiku_id%TYPE, p_kohtade_arv
+Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE, p_kohtade_arv
 Laud.kohtade_arv%type, p_kommentaar Laud.kommentaar%TYPE) IS
-'Protseduuri oodatavad
+'OP3 Protseduuri oodatavad
 sisendid on laua vana identifikaator (parameeter p_laua_kood_vana),
 laua uus identifikaator (parameeter p_laua_kood_uus),
 laua vana identifikaator (parameeter p_laua_kood_vana),
@@ -65,5 +63,20 @@ Protseduur muudab etteantud laua.';
 
 --Public to Private
 REVOKE ALL
-  ON FUNCTION f_laua_kustutamine, f_lisa_laud, f_muuda_laud
+  ON FUNCTION f_laua_unustamine, f_lisa_laud, f_muuda_laud
   FROM PUBLIC ;
+
+--Delete Functions
+
+DROP FUNCTION f_muuda_laud (p_laua_kood_vana
+Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE,
+p_isiku_id Laud.isiku_id%TYPE, p_kohtade_arv
+Laud.kohtade_arv%type, p_kommentaar Laud.kommentaar%TYPE) CASCADE;
+
+DROP FUNCTION f_laua_kustutamine(p_laua_kood
+Laud.laua_kood%TYPE) CASCADE;
+
+DROP FUNCTION f_muuda_laud (p_laua_kood_vana
+Laud.laua_kood%TYPE, p_laua_kood_uus Laud.laua_kood%TYPE,
+p_isiku_id Laud.isiku_id%TYPE, p_kohtade_arv
+Laud.kohtade_arv%type, p_kommentaar Laud.kommentaar%TYPE) CASCADE;
